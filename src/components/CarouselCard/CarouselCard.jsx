7a1289d9e0013c6f0c8672e4839b0a3365b4cbc4
Muneer Hashmat <muneerhashmat@gmail.com>
2025-06-09 10:55:03 +0530

    finish hero section

--- src/components/CarouselCard/CarouselCard.jsx
+++ src/components/CarouselCard/CarouselCard.jsx
@@ -1,13 +1,43 @@
-import { ORIGINAL_IMG_BASE_URL } from "../../utils/constants"
-import "./CarouselCard.css"
+import { CirclePlay } from "lucide-react";
+import {
+  ORIGINAL_IMG_BASE_URL,
+  SMALL_IMG_BASE_URL,
+} from "../../utils/constants";
+import "./CarouselCard.css";
 
-const CarouselCard = ({item}) => {
-    console.log(item)
+const CarouselCard = ({ item }) => {
   return (
-    <div 
-    style={{backgroundImage: `linear-gradient(180deg,transparent 0,transparent 65%,rgba(0,0,0,.35) 83.5%,rgba(0,0,0,.75)), url(${ORIGINAL_IMG_BASE_URL}${item.backdrop_path})`}}
-    className="carousel-card-container"></div>
-  )
-}
+    <div
+      style={{
+        backgroundImage: `linear-gradient(180deg,rgba(0, 0, 0, 0.14) 56%, rgba(10, 13, 12, 1) 86%), url(${ORIGINAL_IMG_BASE_URL}${item.backdrop_path})`,
+      }}
+      className="carousel-card-container"
+    >
+      <div className="carousel-content">
+        <img src={`${SMALL_IMG_BASE_URL}${item.poster_path}`} alt="poster" />
 
-export default CarouselCard
+        <div className="details-container">
+          <div>
+            <CirclePlay size={50} className="play-icon"/>
+          </div>
+          <div>
+            <p 
+            className="title"
+            style={{ fontWeight: "normal" }}>
+              {item.title || item.name}
+            </p>
+            <p 
+            className="overview"
+            style={{ fontWeight: "lighter" }}>
+              {item.overview.length > 200
+                ? item.overview.slice(0, 200) + "..."
+                : item.overview}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselCard;
