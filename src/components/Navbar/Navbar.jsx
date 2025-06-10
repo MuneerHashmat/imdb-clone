d42c56d6530e03239f281bae15f3be2354b04a9f
Muneer Hashmat <muneerhashmat@gmail.com>
2025-06-10 10:42:04 +0530

    created search box for mobile screen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import { BookmarkPlus, ChevronDown, Menu, Search } from "lucide-react";
+import { BookmarkPlus, ChevronDown, Menu, Search, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
   return (
     <nav>
       <button className="menu-tablet nav-item hover">
@@ -17,12 +19,12 @@ const Navbar = () => {
         <p>Menu</p>
       </button>
 
-      <div className="search-bar">
+      <form className="search-bar">
         <input type="text" placeholder="Search IMDb" />
         <button>
           <Search color="gray" />
         </button>
-      </div>
+      </form>
 
       <Link to={"/"} className="imdb-pro link">
         <p>
@@ -59,13 +61,24 @@ const Navbar = () => {
         </div>
 
         <div className="mobile-nav-items">
-          <button>
+          <button onClick={() => setShowMobileSearch(true)}>
             <Search size={20} />
           </button>
           <Link to={"/"} className="link">
             Sign In
           </Link>
           <button className="use-app">Use app</button>
+        </div>
+        <div className={`search-bar-mobile ${showMobileSearch && "visible"}`}>
+          <form>
+            <input type="text" placeholder="Search IMDb" />
+            <button>
+              <Search />
+            </button>
+          </form>
+          <button
+          onClick={()=>setShowMobileSearch(false)}
+          ><X /></button>
         </div>
       </div>
     </nav>
