c13a18d3d120f93d14c9b306a0e5f5dd96ec66cf
Muneer Hashmat <muneerhashmat@gmail.com>
2025-06-12 13:17:26 +0530

    create hero section of content detail page

--- src/pages/ContentDetail/ContentDetail.jsx
+++ src/pages/ContentDetail/ContentDetail.jsx
@@ -2,7 +2,11 @@ import { useParams } from "react-router-dom";
 import "./ContentDetail.css";
 import { useEffect, useState } from "react";
 import { fetchDataFromTMDB } from "./../../api/tmdb";
-import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "./../../utils/constants";
+import {
+  ORIGINAL_IMG_BASE_URL,
+  SMALL_IMG_BASE_URL,
+} from "./../../utils/constants";
+import { formatTime } from "../../utils/utilityFunctions";
 
 const ContentDetail = () => {
   const { type, id } = useParams();
@@ -55,23 +59,80 @@ const ContentDetail = () => {
   return (
     <div className="content-detail-container">
       <div className="content-detail-main">
+        <div
+          className="backdrop"
+          style={
+            details.backdrop_path && {
+              display: "block",
+              backgroundImage: `url(${ORIGINAL_IMG_BASE_URL}${details.backdrop_path})`,
+            }
+          }
+        ></div>
         <div className="details">
-          <div className="basic-details">
-            <h1>{details?.title || details?.name}</h1>
-            <div>
-            <p>{type=="tv" ? "TV Show" : "Movie"}</p><span>|</span>
-            <p>{type=="movie" ? details.release_date.slice(0,4) : details.first_air_date.slice(0,4)}</p><span>|</span>
-            <p
-            style={{
-              color: details.adult ? "red" :"green"
-            }}
-            >{details.adult ? "18+" : "PG-13"}</p>
-            </div>
+          <div className="poster">
+            <img
+              src={
+                details.poster_path
+                  ? `${SMALL_IMG_BASE_URL}${details.poster_path}`
+                  : "/noposter.png"
+              }
+              alt="poster"
+            />
           </div>
-          <div className="poster-and-trailers">
-            <img src={details.poster_path ? `${SMALL_IMG_BASE_URL}${details.poster_path}` : "/poster.png"} alt="" />
-          </div>
+          <div className="main-details">
+            <div className="basic-details">
+              <div className="title-and-tagline">
+                <h1>{details?.title || details?.name}</h1>
+                <p style={{ color: "#bdbdbd" }}>{details.tagline || ""}</p>
+              </div>
+              <div className="general">
+                <p>{type == "tv" ? "TV Show" : "Movie"}</p>
+                <span>|</span>
+                <p>
+                  {type == "movie"
+                    ? details.release_date.slice(0, 4)
+                    : details.first_air_date.slice(0, 4)}
+                </p>
+                <span>|</span>
+                <p
+                  style={{
+                    color: details.adult ? "red" : "green",
+                  }}
+                >
+                  {details.adult ? "18+" : "PG-13"}
+                </p>
+                <span>|</span>
+                {details.vote_average && (
+                  <p
+                  style={{
+                    color:"#f5c518"
+                  }}><img src="/star.png" alt="star" width={15}/>{details.vote_average.toFixed(1)}</p>
+                )}
+              </div>
 
+              <div className="genres">
+                {details.genres && details.genres.map((item)=>(
+                  <div key={item.id}>{item.name}</div>
+                ))}
+              </div>
+            </div>
+            <p className="overview">{details.overview || "No Overview"}</p>
+           <div className="other-details">
+             {details.status && (
+              <div className="other-details-item">
+                <div>
+                <p>Status: <span>{details.status}</span></p>
+                {details.runtime && (
+                  <>
+                    <p>|</p>
+                    <p>Runtime: <span>{formatTime(details.runtime)}</span></p>
+                  </>
+                )}
+                </div>
+              </div>
+            )}
+           </div>
+          </div>
         </div>
       </div>
     </div>
--- src/utils/utilityFunctions.js
+++ src/utils/utilityFunctions.js
@@ -54,3 +54,9 @@ export const isPresentInSessionStorage = (query, page) => {
 
   return item;
 };
+
+export const formatTime = (runtime) => {
+  const hours = Math.floor(runtime / 60);
+  const minutes = Math.floor(runtime % 60);
+  return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+};
